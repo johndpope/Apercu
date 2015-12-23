18826e5c4382f0d483f9b2f92095595c134d7841
dave <lantrip@gmail.com>
2015-12-23 06:06:19 -0500

    Initial commit

---
+++ Apercu/ApercuTests/ApercuTests.swift
@@ -0,0 +1,36 @@
+//
+//  ApercuTests.swift
+//  ApercuTests
+//
+//  Created by David Lantrip on 12/22/15.
+//  Copyright © 2015 Apercu. All rights reserved.
+//
+
+import XCTest
+@testable import Apercu
+
+class ApercuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
