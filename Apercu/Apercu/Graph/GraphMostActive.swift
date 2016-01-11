4f8f365d3c3a3958b0ef39f3e58e1f5844b9cfa5
dave <lantrip@gmail.com>
2016-01-10 20:52:59 -0500

    Most active using new calculation that should be more accurate.  Based off accelerate framework.

--- Apercu/Apercu/Graph/GraphMostActive.swift
+++ Apercu/Apercu/Graph/GraphMostActive.swift
@@ -19,32 +19,74 @@ class GraphMostActive {
         var maxIndexStart = 0
         var maxIndexEnd = 0
         
-        while index < bpm.count {
-            var currentTotal = bpm[index]
-            let startTime = times[index]
+        let timeVector1 = Array(times[1..<times.count])
+        let timeVector2 = Array(times[0..<(times.count - 1)])
+        var deltaTime = [Double](count: timeVector1.count, repeatedValue: 0.0)
+        vDSP_vsubD(timeVector2, 1, timeVector1, 1, &deltaTime, 1, vDSP_Length(timeVector1.count))
+        
+        let bpmVector1 = Array(bpm[1..<bpm.count])
+        let bpmVector2 = Array(bpm[0..<(bpm.count - 1)])
+        var bpmAverageVector = [Double](count: bpmVector1.count, repeatedValue: 0.0)
+        var bpmXtime = [Double](count: bpmVector1.count, repeatedValue: 0.0)
+        var two: Double = 2.0
+        vDSP_vaddD(bpmVector1, 1, bpmVector2, 1, &bpmAverageVector, 1, vDSP_Length(bpmVector1.count))
+        vDSP_vsdivD(bpmAverageVector, 1, &two, &bpmAverageVector, 1, vDSP_Length(bpmVector1.count))
+        
+        // Average of the two samples multiplied by the duration 
+        vDSP_vmulD(bpmAverageVector, 1, deltaTime, 1, &bpmXtime, 1, vDSP_Length(bpmVector1.count))
+        
+        while index < bpmXtime.count {
+            var currentTotal = bpmXtime[index]
+            var timeTotal = deltaTime[index]
             var subIndex = 1
             
-            while (index + subIndex) < bpm.count && (times[index + subIndex] - startTime) < duration {
-                currentTotal += bpm[index + subIndex]
+            while (index + subIndex) < bpmXtime.count && timeTotal < duration {
+                currentTotal += bpmXtime[index + subIndex]
+                timeTotal += deltaTime[index + subIndex]
                 ++subIndex
             }
+            
             --subIndex
             
             if currentTotal > currentMax {
                 currentMax = currentTotal
-                maxIndexStart = index
-                maxIndexEnd = index + subIndex
+                maxIndexStart = index + 1
+                maxIndexEnd = index + subIndex + 1
             }
             
             ++index
         }
         
+//        print(bpmAverageVector)
+//        print(deltaTime)
+//        
+//        print(bpmXtime)
+//        
+//        
+//        while index < bpm.count {
+//            var currentTotal = bpm[index]
+//            let startTime = times[index]
+//            var subIndex = 1
+//            
+//            while (index + subIndex) < bpm.count && (times[index + subIndex] - startTime) < duration {
+//                currentTotal += bpm[index + subIndex]
+//                ++subIndex
+//            }
+//            --subIndex
+//            
+//            if currentTotal > currentMax {
+//                currentMax = currentTotal
+//                maxIndexStart = index
+//                maxIndexEnd = index + subIndex
+//            }
+//            
+//            ++index
+//        }
+        
 //        if maxIndexEnd == bpm.count {
 //            --maxIndexEnd
 //        }
         
-        print(duration)
-        print(times[maxIndexEnd] - times[maxIndexStart])
         completion(timeOne: times[maxIndexStart], timeTwo: times[maxIndexEnd])
     }
 }
